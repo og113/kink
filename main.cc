5ff3a086125685ec24bf78cc7689e42626f7debe
og113 <og113@imperial.ac.uk>
2014-10-31 11:58:08 +0000

    second draft of boundary conditions

--- main.cc
+++ main.cc
@@ -516,11 +516,19 @@ for (unsigned int fileLoop=0; fileLoop<piFiles.size(); fileLoop++)
 			minusDS = Eigen::VectorXd::Zero(2*N*NT+2); //initializing to zero
 			DDS.setZero(); //just making sure
 			Eigen::VectorXi DDS_to_reserve(2*N*NT+2);//number of non-zero elements per column
-			DDS_to_reserve = Eigen::VectorXi::Constant(2*N*NT+2,11);
-			DDS_to_reserve(0) = N+1;
-			DDS_to_reserve(1) = N+1;
-			DDS_to_reserve(2*N*NT-2) = 3;
-			DDS_to_reserve(2*N*NT-1) = 3;
+			DDS_to_reserve = Eigen::VectorXi::Constant(2*N*NT+2,13);
+			if (absolute(theta)<2.0e-16)
+				{
+				DDS_to_reserve(0) = N+3;
+				DDS_to_reserve(1) = 11;
+				}
+			else
+				{
+				DDS_to_reserve(0) = N+11;
+				DDS_to_reserve(1) = N+11;
+				}
+			DDS_to_reserve(2*N*NT-2) = 4;
+			DDS_to_reserve(2*N*NT-1) = 4;
 			DDS_to_reserve(2*N*NT) = N*(zmx.size()-2);
 			DDS_to_reserve(2*N*NT+1) = 2*N*(zmt.size()-2);
 			DDS.reserve(DDS_to_reserve);
@@ -620,93 +628,90 @@ for (unsigned int fileLoop=0; fileLoop<piFiles.size(); fileLoop++)
 					pot_r += Dt*a*Vr(Cp(j));
 					erg(t) += a*pow(Cp(j+1)-Cp(j),2.0)/pow(dt,2.0)/2.0 + pow(Cp(neigh(j,1,1,NT))-Cp(j),2.0)/a/2.0 + a*V(Cp(j)) + a*Vr(Cp(j));
 					
-					//////////////////////////////////////equation R - both///////////////////////////////////
+					//////////////////////////////////////equation I - both///////////////////////////////////
 					for (unsigned int k=1; k<2*2; k++)
 			        	{
 			            int sign = pow(-1,k+1);
 			            int direc = (int)(k/2.0);
 			            if (direc == 0)
 			            	{
-			                minusDS(2*j) += imag(a*Cp(j+sign)/dt));
-			                DDS.coeffRef(2*j,2*(j+sign)) += -imag(a/dt);
-			                DDS.coeffRef(2*j,2*(j+sign)+1) += -real(a/dt);
+			                minusDS(2*j+1) += a*imag(Cp(j+sign)/dt);
+			                DDS.coeffRef(2*j+1,2*(j+sign)) += -imag(a/dt);
+			                DDS.coeffRef(2*j+1,2*(j+sign)+1) += -real(a/dt);
 			                }
 			            else
 			            	{
 			                unsigned int neighb = neigh(j,direc,sign,NT);
-			                minusDS(2*j) += - imag(Dt*Cp(neighb)/a);
-			                DDS.coeffRef(2*j,2*neighb) += imag(Dt)/a;
-			                DDS.coeffRef(2*j,2*neighb+1) += real(Dt)/a;
+			                minusDS(2*j+1) += - imag(Dt*Cp(neighb)/a);
+			                DDS.coeffRef(2*j+1,2*neighb) += imag(Dt)/a;
+			                DDS.coeffRef(2*j+1,2*neighb+1) += real(Dt)/a;
 			                }
 			            }
 			        comp temp0 = a/dt - 2.0*Dt/a;
+			        comp temp1 = a*Dt*( dV(Cp(j)) + dVr(Cp(j)) );//dV terms should be small
 			        
-			        minusDS(2*j) += -imag(temp0*Cp(j)) + a*imag( Dt*( dV(Cp(j)) + dVr(Cp(j)) ) );
-			        DDS.coeffRef(2*j,2*j) += imag(temp0) - a*imag( Dt*( ddV(Cp(j)) + ddVr(Cp(j)) ) );
-			        DDS.coeffRef(2*j,2*j+1) += real(temp0) - a*real( Dt*( ddV(Cp(j)) + ddVr(Cp(j)) ) );
+			        minusDS(2*j+1) += imag(-temp0*Cp(j) + temp1 );
+			        DDS.coeffRef(2*j+1,2*j) += imag(temp0 - temp1 );
+			        DDS.coeffRef(2*j+1,2*j+1) += real(temp0 - temp1 );
 				    /////////////////////////////////////////////////////////////////////////////////////////
 					if (absolute(theta)<2.0e-16)
 						{
 						//simplest boundary conditions replaced by ones continuously connected to theta!=0 ones
-						//DDS.insert(2*j,2*(j+1)+1) = 1.0; //zero imaginary part of time derivative
-						//DDS.insert(2*j+1,2*j+1) = 1.0; //zero imaginary part
+						//DDS.insert(2*j+1,2*(j+1)+1) = 1.0; //zero imaginary part of time derivative
+						//DDS.insert(2*j,2*j+1) = 1.0; //zero imaginary part
 						
-						/////////////////////////////////////equation I - theta=0//////////////////////////////////////
+						/////////////////////////////////////equation R - theta=0//////////////////////////////////////
 						for (unsigned int k=0;k<N;k++)
 							{
 							if (absolute(omega(x,k))>2.0e-16)
 								{
 								unsigned int m=k*NT;
-								DDS.coeffRef(2*j+1,2*m+1) += -2.0*omega(x,k);
-								minusDS(2*j+1) += 2.0*omega(x,k)*p(2*m+1);
+								DDS.coeffRef(2*j,2*m+1) += -2.0*omega(x,k);
+								minusDS(2*j) += 2.0*omega(x,k)*p(2*m+1);
 								}
 							}
 						////////////////////////////////////////////////////////////////////////////////////////
 						}
 					else
 						{
-						//////////////////////////////////////equation R - theta!=0/////////////////////////////
+						//////////////////////////////////////equation I - theta!=0/////////////////////////////
 						for (unsigned int k=0;k<N;k++)
 							{
 							if (absolute(omega(x,k))>2.0e-16)
 								{
 								unsigned int m=k*NT;
-								DDS.coeffRef(2*j,2*m) += (1.0-Gamma)*omega(x,k)/(1.0+Gamma);
-								minusDS(2*j) += -(1.0-Gamma)*omega(x,k)*(p(2*m)-root[0])/(1.0+Gamma);
+								DDS.coeffRef(2*j+1,2*m) += (1.0-Gamma)*omega(x,k)/(1.0+Gamma);
+								minusDS(2*j+1) += -(1.0-Gamma)*omega(x,k)*(p(2*m)-root[0])/(1.0+Gamma);
 								}
 							}
 						/////////////////////////////////////////////////////////////////////////////////////////
-						//////////////////////////////////////equation I - theta!=0//////////////////////////////
+						//////////////////////////////////////equation R - theta!=0//////////////////////////////
 						for (unsigned int k=1; k<2*2; k++)
 				        	{
 				            int sign = pow(-1,k+1);
 				            int direc = (int)(k/2.0);
 				            if (direc == 0)
 				            	{
-				                minusDS(2*j+1) += imag(a/dt)*p(2*(j+sign)) + real(a/dt)*p(2*(j+sign)+1);
-			                	DDS.coeffRef(2*j+1,2*(j+sign)) += -real(a/dt)*theta;
-			                	DDS.coeffRef(2*j+1,2*(j+sign)+1) += imag(a/dt)*theta;
+				                minusDS(2*j) += a*real(Cp(j+sign)/dt)*theta;
+			                	DDS.coeffRef(2*j,2*(j+sign)) += -real(a/dt)*theta;
+			                	DDS.coeffRef(2*j,2*(j+sign)+1) += imag(a/dt)*theta;
 				                }
 				            else
 				            	{
 				                unsigned int neighb = neigh(j,direc,sign,NT);
-				                minusDS(2*j) += - real(Dt*Cp(neighb)/a);
-				                minusDS(2*j+1) += - imag(Dt*Cp(neighb)/a);
-				                DDS.coeffRef(2*j+1,2*(j+sign)) += -real(a/dt)*theta;
-			                	DDS.coeffRef(2*j+1,2*(j+sign)+1) += imag(a/dt)*theta;
+				                minusDS(2*j) += - real(Dt*Cp(neighb))*theta/a;
+				                DDS.coeffRef(2*j,2*(j+sign)) += real(Dt)*theta/a;
+			                	DDS.coeffRef(2*j,2*(j+sign)+1) += -imag(Dt)*theta/a;
 				                }
 				            }
-		            	comp temp1 = a*Dt*(2.0*Cp(j)/pow(a,2.0) + dV(Cp(j))) + dVr(Cp(j)));//dV terms should be small
-
-				        minusDS(2*j+1) += real(-temp0*Cp(j) + a*Dt*( dV(Cp(j)) + dVr(Cp(j)) ) )*theta;
-			        	DDS.coeffRef(2*j+1,2*j) += real(temp0 - a*Dt*( ddV(Cp(j)) + ddVr(Cp(j)) ) )*theta;
-			        	DDS.coeffRef(2*j+1,2*j+1) += imag(-temp0 + a*Dt*( ddV(Cp(j)) + ddVr(Cp(j)) ) )*theta;
-
+				        minusDS(2*j) += real(-temp0*Cp(j) + temp1 )*theta;
+			        	DDS.coeffRef(2*j,2*j) += real(temp0 - temp1 )*theta;
+			        	DDS.coeffRef(2*j,2*j+1) += imag(-temp0 + temp1 )*theta;
                         for (unsigned int k=0; k<N; k++)
                         	{
 							unsigned int n = k*NT;
-							minusDS(2*j+1) += (1+Gamma)*omega(x,k)p(2*n+1)*theta/(1-Gamma);
-							DDS.coeffRef(2*j,2*n+1) += -omega(x,k)*(1.0-Gamma)/(1.0+Gamma)*theta;
+							minusDS(2*j) += (1+Gamma)*omega(x,k)*p(2*n+1)*theta/(1-Gamma);
+							DDS.coeffRef(2*j,2*n+1) += -omega(x,k)*(1.0+Gamma)/(1.0-Gamma)*theta;
 							bound += -(1.0-Gamma)*omega(x,k)*(p(2*j)-root[0])*(p(2*n)-root[0])/(1.0+Gamma) + (1.0+Gamma)*omega(x,k)*p(2*j+1)*p(2*n+1)/(1.0-Gamma);
                         	}
 						}
