1ff57125182482848d8d2dff2efef392b370e411
Oli Gould <og113@imperial.ac.uk>
2014-11-19 16:24:40 +0000

    added sphaleron.cc

--- pf.h
+++ pf.h
@@ -32,7 +32,6 @@ typedef Eigen::MatrixXd mat;
 typedef Eigen::MatrixXcd cMat;
 typedef Eigen::VectorXd vec;
 typedef Eigen::VectorXcd cVec;
-typedef Eigen::Triplet<double> triplet;
 
 complex<double> i(0.0,1.0);
 #define pi 3.14159265359
@@ -670,20 +669,26 @@ comp dtFn (const unsigned int& time)
 	
 comp DtFn (const unsigned int& time)
 	{
-	comp xDt;
-	if (time==(NT-1))
+	return a;
+	}
+	
+double dxFn (const unsigned int& time)
+	{
+	return a;
+	}
+	
+double DxFn (const unsigned int& time)
+	{
+	comp xdt;
+	if (time<(NT-1))
 		{
-		xDt = (simpleTime(time)-simpleTime(time-1))/2.0;
-		}
-	else if (time==0)
-		{
-		xDt = (simpleTime(time+1) - simpleTime(time))/2.0;
+		xdt = simpleTime(time+1)-simpleTime(time);
 		}
 	else
 		{
-		xDt = (simpleTime(time+1)-simpleTime(time-1))/2.0;
+		xdt = 0;
 		}
-	return xDt;
+	return xdt;
 	}
 	
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -910,21 +915,6 @@ void gpSimple(const string & readFile)
 	fprintf(gnuplotPipe, "%s \n", command2);
 	pclose(gnuplotPipe);
 	}
-	
-//print repi via gnuplot
-void gpSimple2(const string & readFile) 
-	{
-	string commandOpenStr = "gnuplot -persistent";
-	const char * commandOpen = commandOpenStr.c_str();
-	FILE * gnuplotPipe = popen (commandOpen,"w");
-	string command1Str = "plot \"" + readFile + "\" using 1:2 with linespoints";
-	string command2Str = "pause -1";
-	const char * command1 = command1Str.c_str();
-	const char * command2 = command2Str.c_str();
-	fprintf(gnuplotPipe, "%s \n", command1);
-	fprintf(gnuplotPipe, "%s \n", command2);
-	pclose(gnuplotPipe);
-	}
 
 	
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -968,7 +958,7 @@ vec loadVector (const string& loadFile, const unsigned int& Nt, const unsigned i
 		}
 	if ((j+k)!=(Nt*Nx+zeroModes))
 		{
-		cout << "loadVector error in: << " << loadFile << endl;
+		cout << "loadVector error in: " << loadFile << endl;
 		cout << "j+k = " << j+k << endl;
 		}
 	F.close();
