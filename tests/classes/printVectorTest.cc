5745198ea31de8b2b146b1377acdc7d1205879d3
Oli Gould <og113@imperial.ac.uk>
2015-01-15 00:10:43 +0000

    added some more classes files

--- tests/classes/printVectorTest.cc
+++ tests/classes/printVectorTest.cc
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 using namespace std;
@@ -27,6 +28,27 @@ ostream& operator<<(ostream& os, const myVec& vec) {
 	}
 }
 
+ostream& operator<<(ostream& os, const vector< vector<double> >& vecVec) {
+	uint size = vecVec[0].size();
+	if (vecVec.size()>0) {
+		for (uint j=1; j<vecVec.size(); j++) {
+			if (vecVec[j].size()!=size) throw "print error";
+		}
+		for (uint k=0; k<size; k++) {
+			for (uint j=0; j<vecVec.size(); j++) {
+				os << setw(16) << vecVec[j][k];
+			}
+			os << endl;
+		}
+	}
+	else {
+		for (uint k=0; k<size; k++) {
+			os << vecVec[0][k] << endl;
+		}
+	}
+	return os;
+}
+
 int main(){
 
 const double tmpu[] = { 4.0, 5.0, 6.0 };
@@ -40,6 +62,11 @@ ofstream f;
 f.open("outputs");
 f << myv;
 f.close();
+
+vector< vector<double> > w(2);
+w[0] = u;
+w[1] = v;
+cout << w;
 
 return 0;
 }
